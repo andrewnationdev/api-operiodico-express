4240b9e5a99d5350bf19877239a7b37020e1ca5a
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-04-10 19:31:41 +0000

    🔒️ CORS

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const cors = require('cors');
 const app = express()
 const port = 3000
 
-app.use(cors());
+app.use((req, res, next) =>  {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    next();
+});
 
 const articles = [
   {
